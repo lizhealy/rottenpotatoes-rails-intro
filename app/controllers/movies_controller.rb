5d60a0b990a3c1bc713896f742e284c7f09a4490
lizhealy <lizhealy8@gmail.com>
2015-10-11 18:46:35 +0000

    HW Part 3 complete by Liz Healy

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,7 +13,57 @@ class MoviesController < ApplicationController
   end
 
   def index
-    
+    @movies = Movie.all
+    @sorter = 0
+    @release_date_sort = ''
+    @title_sort = ''
+
+
+    if(@the_ratings != nil)
+      @movies = @movies.find_all{ |movie| @the_ratings.has_key?(movie.rating) and @the_ratings[movie.rating]==true}
+    end
+
+
+    if(params[:sort].to_s == 'title')
+      session[:sort] = params[:sort]
+      @title_sort = 'hilite'
+      @release_date_sort = ''
+      @movies = @movies.sort_by{|movie| movie.title.to_s}
+    elsif(params[:sort].to_s == 'release')
+      session[:sort] = params[:sort]
+      @release_date_sort = 'hilite'
+      @title_sort = ''
+      @movies = @movies.sort_by{|movie| movie.release_date.to_s}
+    elsif(session.has_key?(:sort))
+      params[:sort] = session[:sort]
+      @sorter = 1
+    end
+
+
+    if(params[:ratings]!=nil)
+      session[:ratings] = params[:ratings]
+      @movies = @movies.find_all{ |movie| params[:ratings].has_key?(movie.rating)}
+    elsif(session.has_key?(:ratings))
+      params[:ratings] = session[:ratings]
+      @sorter = 1
+    end
+
+
+    if(@sorter == 1)
+      redirect_to movies_path(:sort=>params[:sort], :ratings=>params[:ratings])
+    end
+
+    @the_ratings = {}
+    @all_ratings = ['G','PG','PG-13','R','NC-17']
+
+
+    @all_ratings.each do |rating, value|
+      if params[:ratings] == nil
+        @the_ratings[rating] = true
+      else
+        @the_ratings[rating] = params[:ratings].has_key?(rating)
+      end
+    end
   end
 
   def new
